6bd4c843d9bc350aa0c59fb54dd5acc0dcd4d7dc
Ceejay-777 <covenantcrackslord01@gmail.com>
2024-01-30 16:03:47 +0100

    commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        paleViolet: "hsl(276, 100%, 81%)",
+        moderateViolet: "hsl(276, 55%, 52%)",
+        desaturatedDarkViolet: "hsl(271, 15%, 43%)",
+        grayishBlue: "hsl(206, 6%, 79%)",
+        veryDarkDesaturatedViolet: "hsl(271, 36%, 24%)",
+        darkGrayishViolet: "hsl(270, 7%, 64%)",
+        lightGrayishViolet: "hsl(270, 20%, 96%)",
+        veryLightMagenta: "hsl(289, 100%, 72%)",
+        lightMagenta: "hsl(293, 100%, 63%)",
+        lightViolet: "hsl(264, 100%, 61%)",
+      },
+      borderRadius: {
+        mobile: "0% 0% 100% 0% / 0% 0% 35% 0% ",
+        desktop: "0% 0% 45% 40% / 0% 0% 25% 14% ",
+      },
+    },
   },
   plugins: [],
 };
